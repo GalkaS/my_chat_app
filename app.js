cbca6073132f4348956ba2c7603e6ac196363213
Gala <galagalunia@gmail.com>
2017-04-17 19:11:54 -0400

    Publish to github

---
+++ app.js
@@ -0,0 +1,15 @@
+var socket = io();
+
+$('form').submit(function () {
+  var text = $('#m').val();
+
+  socket.emit('message', text);
+  $('#m').val('');
+
+  return false;
+});
+
+socket.on('message', function (msg) {
+	var incomingMessage = $('<li>').text(msg);
+	$('#history').append(incomingMessage);
+});
---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+
+var http = require('http');
+var server = http.Server(app);
+
+app.use(express.static('client'));
+
+var io = require('socket.io')(server);
+
+io.on('connection', function (socket) {
+  socket.on('message', function (msg) {
+    io.emit('message', msg);
+  });
+});
+
+/*server.listen(process.env.PORT, process.env.IP, function () {
+  var addr = server.address();
+  console.log("Chat server running at", addr.address + ":" + addr.port);
+});*/
+
+server.listen(8080, 'localhost', function () {
+	var addr = server.address();
+	console.log("Chat server running at", addr.address + ":" + addr.port);
+});
